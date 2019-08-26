844c1062366180556219bb8bd910ad8237c2eca3
Diederik Eenschooten <diederikeen@gmail.com>
2019-08-26 14:59:33 +0200

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,12 +1,1 @@
-module.exports = {
-  google: {
-    clientID: '537556395654-72eu2p57euh6bbeg62ccpneqlmpe7qei.apps.googleusercontent.com',
-    clientSecret: '8U0V4y3RTpjpcFytP9ScL3jG',
-  },
-  session: {
-    cookieKey: 'Zcb0rOpKVkLIcNwl',
-  },
-  cryptr: {
-    key: 'secretfinanceappkey',
-  },
-};
+
